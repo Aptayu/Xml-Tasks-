acbaa40d5148f866b9cce8e27e7d2b7e712aba43
Arpit <arpitarga@gmail.com>
2019-10-04 02:21:23 +0530

    Third commit

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,7 +1,36 @@
-package com.stackroute.domain;
+ package com.stackroute.domain;
 
-public class Movie {
-    private Actor actor;
+ import org.springframework.beans.BeansException;
+ import org.springframework.beans.factory.BeanFactory;
+ import org.springframework.beans.factory.BeanFactoryAware;
+ import org.springframework.beans.factory.BeanNameAware;
+ import org.springframework.context.ApplicationContext;
+ import org.springframework.context.ApplicationContextAware;
+
+ public class Movie implements ApplicationContextAware, BeanNameAware, BeanFactoryAware {
+     private Actor actor;
+     ApplicationContext context;
+     private BeanFactory beanFactory;
+     private String beanName;
+
+     @Override
+     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+     }
+
+     @Override
+     public void setBeanName(String s) {
+            this.beanName = s;
+     }
+
+     @Override
+     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            this.context = context;
+     }
+
+
+
+
 //
 //    public Movie(Actor actor) {
 //        this.actor = actor;
@@ -15,7 +44,6 @@ public class Movie {
         this.actor = actor;
     }
 
-    @Override
     public String toString() {
         return "Movie{" +
                 "actor=" + actor.toString()+
